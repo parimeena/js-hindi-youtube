5b446ca6e26be5de6f5c52525c70a3514dcd9240
parimeena <167202093+parimeena@users.noreply.github.com>
2025-01-24 18:34:08 +0000

    25jan

---
+++ 1_basics/3_NumsAndMaths.js
@@ -0,0 +1,24 @@
+const score =100
+console.log(score);
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+const otherNumber = 23.8954;
+
+console.log(otherNumber.toPrecision(3));
+const hundreds = 100000
+console.log(Math);
+console.log(hundreds.toLocaleString('en-In'))
+console.log(Math.abs(4));
+console.log(Math.round(4.6))
+console.log(Math.min(4,3,67,86));
+console.log(Math.max(4,3,67,86));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min =10
+const max =20
+console.log(Math.random()* (max-min+1))+min);
+
+
