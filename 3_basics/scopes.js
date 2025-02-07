96f99e6a629ec256385ef24edd978fbabb052b54
parimeena <167202093+parimeena@users.noreply.github.com>
2025-02-07 18:14:48 +0000

    7jan

---
+++ 3_basics/scopes.js
@@ -0,0 +1,52 @@
+function one()
+{const username ="pari"
+function two()
+{
+const website ="youtube"
+console.log(username)
+    }
+    two()}
+one()
+if(true)
+{const username ="pari"
+if(username === "pari")
+    {const website ="youtube"
+        console.log(username + website);}
+    //  console.log(website);
+}
+//console.log(username);
+// we cannot get the value outside the scope
+const username={
+  username:"pari",
+  price:999.,
+  welcomeMessage:function()
+  {
+    console.log(`${this.username}, welcome to website`);
+    console.log(this);
+  }
+}
+// username.welcomeMessage()
+// username.username="sam"
+// username.welcomeMessage()
+console.log(this.username);
+// const chai= () => {
+//  let username="meena";
+//  console.log(this.username);
+// }
+// chai()
+// const addTwo=(num1,num2)=>
+// {
+//     return num1+num2
+// }
+// console.log(addTwo(3,4))
+const addTwo=(num1,num2)=>num1+num2
+console.log(addTwo(3,4));
+const myArray =[2,5,3,5,7,9]
+{function chai()
+{
+    console.log("DB connected");
+}}{}
+chai()
+(() =>{
+    console.log();
+})()
